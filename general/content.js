d20763ba9d22c23b39896143e5ff83e3d68f249f
Hunter <fam@logan.local>
2026-01-18 20:50:34 -0500

    added things which generally work

---
+++ general/content.js
@@ -0,0 +1,18 @@
+// embed a page
+link = prompt("Link:");
+document.getElementsByTagName("body")[0].innerHTML = `
+<iframe src="${link}" style="border:none;position:fixed;inset:0;width:100vw;height:100vh"></iframe>`;
+/*
+javascript:link=prompt("Link:");document.getElementsByTagName("body")[0].innerHTML=`<iframe src="${link}" style="border:none;position:fixed;inset:0;width:100vw;height:100vh"></iframe>`;
+*/
+// delete body
+document.getElementsByTagName("body")[0].innerHTML = "";
+/*
+javascript:document.getElementsByTagName("body")[0].innerHTML="";
+*/
+// delete EVERYTHING (should be no trace of anything except for localStorage and cookies)
+Object.keys(window).forEach(e => {if (e !== "location") window[e] = undefined});
+document.write("");
+/*
+javascript:Object.keys(window).forEach(e=>{if(e!=="location")window[e]=undefined});document.write("");
+*/
---
+++ general/titles.js
@@ -0,0 +1,5 @@
+// change the title (super simple)
+document.title = prompt("New title:");
+/*
+javascript:document.title=prompt("New title:");
+*/
